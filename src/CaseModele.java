e4c41f4231da636047534171c3cd6fa93e69043f
Elodie Bourbon <elodie.bourbon@etu.univ-lyon1.fr>
2021-11-24 16:33:52 +0100

    modif modele
    deplacement infos vueCase lié au modèle dans CaseModele

--- src/CaseModele.java
+++ src/CaseModele.java
@@ -1,5 +1,33 @@
 import java.util.Observable;
+import java.util.Random;
 
-public class CaseModele extends Observable{
+public class CaseModele{
+    private int x, y;
+    private CaseType type;
+	private static Random rnd = new Random();
+    
+    public CaseModele(int _x, int _y) {
+        x = _x;
+        y = _y;
+    }
 
+    public void rndType() {
+    	type = CaseType.values()[rnd.nextInt(CaseType.values().length)];
+    }
+    
+    public void setType(CaseType t) {
+    	type = t;
+    }
+    
+    public CaseType getType() {
+    	return type;
+    }
+    
+    public int getX() {
+    	return x;
+    }
+    
+    public int getY() {
+    	return y;
+    }
 }
